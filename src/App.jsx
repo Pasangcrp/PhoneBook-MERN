5d344371e124f1bc2dd2d8f69181330b20381e0b
Pasang Tshering Sherpa <111878163+Pasangcrp@users.noreply.github.com>
2024-01-02 22:48:59 +0545

    edited the codes for phonebook by modifying pb.json and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const App = () => {
-  return <h1>Hello</h1>;
+  const [contacts, setContacts] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/contacts").then((response) => {
+      setContacts(response.data);
+    });
+  }, []);
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handlePhoneChange = (e) => {
+    setNewPhone(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newContact = { name: newName, number: newPhone };
+
+    axios
+      .post("http://localhost:3001/contacts", newContact)
+      .then((response) => {
+        setContacts(contacts.concat(response.data));
+        setNewName("");
+        setNewPhone("");
+      });
+  };
+
+  return (
+    <div>
+      <h1>PhoneBook</h1>
+      <form onSubmit={handleFormSubmit}>
+        Name: <input type="text" value={newName} onChange={handleNameChange} />
+        Phone Number:{" "}
+        <input type="tel" value={newPhone} onChange={handlePhoneChange} />
+        <button type="submit">Add</button>
+      </form>
+      <h2>Numbers:</h2>
+      <ul>
+        {contacts.map((contact, index) => (
+          <li key={index}>
+            {contact.name}: {contact.number}{" "}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
